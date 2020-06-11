1a2aebe3bafe3eade90f44fb22c99accb8463a17
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-06-11 11:25:37 -0500

    feature: created database and tables

---
+++ migration.sql
@@ -0,0 +1,23 @@
+create database if not exists adlister_db;
+
+use adlister_db;
+
+drop table if exists ads;
+drop table if exists users;
+
+create table users (
+    id int unsigned not null auto_increment,
+    username varchar(240) not null,
+    email varchar(240) not null,
+    password varchar(240),
+    primary key (id)
+);
+
+create table ads (
+    id int unsigned not null auto_increment,
+    user_id int unsigned not null,
+    title varchar(240) not null,
+    description text not null,
+    foreign key (user_id) references users (id)
+        on delete cascade
+);
