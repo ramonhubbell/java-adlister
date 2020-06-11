405664ea6231339b519a3f323d3e1f09c0fcfb5b
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-06-11 12:31:55 -0500

    created MySQLAdsDao and implemented Ads interface

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,56 @@
+import java.sql.*;
+import java.util.List;
+import com.mysql.cj.jdbc.Driver;
+
+public class MySQLAdsDao implements Ads{
+    private Connection connection = null;
+
+    public MySQLAdsDao (Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+@Override
+    public List<Ad> all() {
+    try {
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("select * from ads" );
+        while (rs.next()) {
+            System.out.println(rs.getLong("id"));
+            System.out.println(rs.getString("title"));
+            System.out.println(rs.getString("description"));
+        }
+    } catch (SQLException throwables){
+        throwables.printStackTrace();
+    }
+    return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        String query = String.format("insert into ads (user_id, title, description) values (%s, %s, %s)",
+                ad.getUserId() + ad.getTitle() + ad.getDescription());
+        System.out.println(query);
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = statement.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+}
+
+
